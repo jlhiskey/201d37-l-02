efcea25d2205a7eb9dd7ff9032fde030124fa0b3
jlhiskey <jasonhi@crazyredhead.com>
2018-07-11 13:51:37 -0700

    Fixed after receiving grading comments.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,58 @@
-alert('Please answer all questions using Y or N')
-    var answerOne = prompt('Was I born in WA?');
+'use strict';
+
+alert('Hello!!! My name is Jason. You are about to guess who I am. Please answer all questions using Y or N or Yes or No')
+    var answerOne = prompt('Was I born in WA?').toUpperCase();
     console.log('Was I born in WA?', answerOne);
 
-    if (answerOne.toUpperCase() === 'Y') {
+    if (answerOne === 'Y' || answerOne === 'YES') {
        alert('Wrong');
-    } else if (answerOne.toUpperCase() ==='N') {
+    } else if (answerOne ==='N' || answerOne === 'NO') {
         alert('Correct');
-    } 
+    } else {
+        alert('Try inputing Y/N or YES/NO');
+    }
 
-    var answerTwo = prompt('Am I over 30 years old?');
+    var answerTwo = prompt('Am I over 30 years old?').toUpperCase();
     console.log('Am I over 30 years old?', answerTwo);
 
-   if (answerTwo.toUpperCase() === 'Y') {
+   if (answerTwo === 'Y' || answerTwo === 'YES') {
        alert('Correct');
-    } else if (answerTwo.toUpperCase() ==='N') {
+    } else if (answerTwo ==='N' || answerTwo === 'NO') {
         alert('Wrong');
-    } 
+    } else {
+        alert('Try inputing Y/N or YES/NO');
+    }
 
-    var answerThree = prompt('Do I own a Ford?');
+    var answerThree = prompt('Do I own a Ford?').toUpperCase();
     console.log('Do I own a Ford?', answerThree);
 
-    if (answerThree.toUpperCase() === 'Y') {
+    if (answerThree === 'Y' || answerThree === 'YES') {
         alert('Definentaly incorrect');
-     } else if (answerThree.toUpperCase() ==='N') {
+    } else if (answerThree ==='N' || answerThree === 'NO') {
          alert('Chevy all the way!!!');
-    } 
+    } else {
+        alert('Try inputing Y/N or YES/NO');
+    }
 
-    var answerFour = prompt('Do I own a banjo?');
+    var answerFour = prompt('Do I own a banjo?').toUpperCase();
     console.log('Do I own a banjo?', answerFour);
 
-    if (answerFour.toUpperCase() === 'Y') {
+    if (answerFour === 'Y' || answerFour === 'YES') {
         alert('Correct');
-     } else if (answerFour.toUpperCase() ==='N') {
+    } else if (answerFour ==='N' || answerFour === 'NO') {
          alert('Wrong');
-     }
+    } else {
+        alert('Try inputing Y/N or YES/NO');
+    }
 
-    var answerFive = prompt('Do I have any idea how to write code?');
+    var answerFive = prompt('Do I have any idea how to write code?').toUpperCase();
     console.log('Do I have any idea how to write code?', answerFive);
 
-    if (answerFive.toUpperCase() === 'Y') {
+    if (answerFive === 'Y' || answerFive === 'YES') {
         alert('Thanks for the compliment but I am completely lost.');
-    } else if (answerFive.toUpperCase() ==='N') {
-        alert('Correct I am completely lost.');        
-};  
-
+    } else if (answerFive ==='N' || answerFive === 'NO') {
+        alert('Correct I am completely lost.');
+    } else {
+        alert('Try inputing Y/N or YES/NO')
+    }
+          
